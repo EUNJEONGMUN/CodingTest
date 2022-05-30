d19662cd2d9e18ab31ce23b84e43ba9224c16bf8
EUNJEONGMUN <munej26@gmail.com>
2022-05-30 14:02:29 +0900

    [ADD] complete 220530 commit:

---
+++ Tree/1068.py
@@ -0,0 +1,63 @@
+# from collections import defaultdict
+
+
+# def remove(x):  # x 노드의 자식들 삭제
+#     if x in tree:
+#         for i in tree[x]:
+#             remove(i)
+#         del(tree[x])
+
+
+# n = int(input())
+# graph = list(map(int, input().split()))
+# tree = defaultdict(list)  # key: 부모, value:자식
+# for i in range(n):
+#     tree[graph[i]].append(i)
+
+# delete = int(input())
+# remove(delete)
+# cnt = 0
+
+# tree[graph[delete]].remove(delete)
+
+# for key, value in tree.items():
+#     if key == -1:
+#         continue
+#     for i in value:
+#         if i not in tree and i != delete:
+#             cnt += 1
+
+# print(cnt)
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+delete = int(input())
+tree = [[] for _ in range(n)]
+start = 0
+for i in range(len(arr)):
+    if arr[i] == -1:
+        start = i
+    else:
+        tree[arr[i]].append(i)
+
+cnt = 0
+
+
+def dfs(v):
+    global cnt
+    if v == delete:
+        return
+    if len(tree[v]) == 0:
+        cnt += 1
+    elif len(tree[v]) == 1 and delete in tree[v]:
+        cnt += 1
+
+    else:
+        for i in tree[v]:
+            if i != delete:
+                dfs(i)
+
+
+dfs(start)
+print(cnt)
