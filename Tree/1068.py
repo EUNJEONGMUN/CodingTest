edf6ea92e8953e057e7cf89bda6a8f760618b18d
EUNJEONGMUN <munej26@gmail.com>
2022-05-30 14:04:40 +0900

    [CHORE] add comment

--- Tree/1068.py
+++ Tree/1068.py
@@ -39,22 +39,22 @@ for i in range(len(arr)):
     if arr[i] == -1:
         start = i
     else:
-        tree[arr[i]].append(i)
+        tree[arr[i]].append(i)  # 부모노드 -> 자식노드
 
 cnt = 0
 
 
 def dfs(v):
     global cnt
-    if v == delete:
+    if v == delete:  # 노드가 삭제될 노드이면 return
         return
-    if len(tree[v]) == 0:
-        cnt += 1
-    elif len(tree[v]) == 1 and delete in tree[v]:
+    if len(tree[v]) == 0:  # 자식노드가 없다면
+        cnt += 1  # cnt+=1
+    elif len(tree[v]) == 1 and delete in tree[v]:  # 자식 노드가 1개이지만, 삭제 된 노드이면 cnt+=1
         cnt += 1
 
     else:
-        for i in tree[v]:
+        for i in tree[v]:  # 삭제 되지 않은 노드 dfs 탐색
             if i != delete:
                 dfs(i)
 
