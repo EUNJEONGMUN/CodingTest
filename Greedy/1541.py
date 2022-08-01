8ea2226b888dae7f00c517cdd4d5fc4f50c88879
EUNJEONGMUN <munej26@gmail.com>
2022-08-01 17:29:55 +0900

    [CHORE] add comment

--- Greedy/1541.py
+++ Greedy/1541.py
@@ -1,9 +1,11 @@
-s = input().split("-")
+# 1541
+
+s = input().split("-")  # '-' 로 나누기
 res = 0
 for i in range(len(s)):
-    temp = sum(map(int, s[i].split("+")))
-    if i == 0:
+    temp = sum(map(int, s[i].split("+")))  # '+'로 연결된 부분 계산하기
+    if i == 0:  # 첫 번째에 있는 요소일 때 더해주기
         res += temp
-    else:
+    else:  # 나머지는 다 빼주기
         res -= temp
 print(res)
--- Greedy/20365.py
+++ Greedy/20365.py
@@ -8,10 +8,10 @@ if s[0] == "B":
     blue += 1
 else:
     red += 1
-for i in range(1, n):
-    if s[i-1] != s[i]:
-        if s[i] == "B":
+for i in range(1, n):  # 구역의 갯수 세기
+    if s[i-1] != s[i]:  # 앞과 뒤가 다를 때
+        if s[i] == "B":  # B면 blue 추가
             blue += 1
-        else:
+        else:  # R이면 red 추가
             red += 1
 print(min(blue, red)+1)
