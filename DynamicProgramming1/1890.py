d21c264e008c37013dbadb042fa3c627d901a3a6
EUNJEONGMUN <munej26@gmail.com>
2022-08-08 15:15:59 +0900

    [CHORE] edit comment

--- DynamicProgramming1/1890.py
+++ DynamicProgramming1/1890.py
@@ -37,7 +37,7 @@ input = sys.stdin.readline
 n = int(input())
 table = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0]*n for _ in range(n)]
-dp[0][0] = 1
+dp[0][0] = 1  # 초기화
 for i in range(n):
     for j in range(n):
         if i == n-1 and j == n-1:
