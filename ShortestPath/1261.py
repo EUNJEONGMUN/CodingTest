bee12effa5a5c564f792484ff3d7267e5fb13dc1
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-10 11:17:04 +0900

    add: boj 1261 알고 스팟 풀이 완료 - 최단경로 풀이

--- ShortestPath/1261.py
+++ ShortestPath/1261.py
@@ -1,4 +1,51 @@
-from collections import deque
+# bfs 풀이
+
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# m, n = map(int, input().split())
+# matrix = list(list(input().strip()) for _ in range(n))
+# INF = int(1e9)
+# dist = [[INF]*m for _ in range(n)]
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+
+# def check_range(x, y):
+#     if x >= 0 and y >= 0 and x < n and y < m:
+#         return True
+#     return False
+
+
+# def solution():
+#     q = deque()
+#     dist[0][0] = 0
+#     q.append((0, (0, 0)))
+
+#     while q:
+#         cnt, position = q.popleft()
+
+#         if cnt > dist[position[0]][position[1]]:
+#             continue
+
+#         for i in range(4):
+#             nx, ny = position[0]+dx[i], position[1]+dy[i]
+#             if check_range(nx, ny):
+#                 if matrix[nx][ny] == "1" and dist[nx][ny] > cnt+1:
+#                     dist[nx][ny] = cnt+1
+#                     q.append((cnt+1, (nx, ny)))
+#                 elif matrix[nx][ny] == "0" and dist[nx][ny] > cnt:
+#                     dist[nx][ny] = cnt
+#                     q.append((cnt, (nx, ny)))
+
+
+# solution()
+# print(dist[-1][-1])
+
+# 최단경로 풀이
+import heapq
 import sys
 input = sys.stdin.readline
 
@@ -18,12 +65,12 @@ def check_range(x, y):
 
 
 def solution():
-    q = deque()
+    q = []
     dist[0][0] = 0
-    q.append((0, (0, 0)))
+    heapq.heappush(q, (0, (0, 0)))
 
     while q:
-        cnt, position = q.popleft()
+        cnt, position = heapq.heappop(q)
 
         if cnt > dist[position[0]][position[1]]:
             continue
@@ -33,10 +80,10 @@ def solution():
             if check_range(nx, ny):
                 if matrix[nx][ny] == "1" and dist[nx][ny] > cnt+1:
                     dist[nx][ny] = cnt+1
-                    q.append((cnt+1, (nx, ny)))
+                    heapq.heappush(q, (cnt+1, (nx, ny)))
                 elif matrix[nx][ny] == "0" and dist[nx][ny] > cnt:
                     dist[nx][ny] = cnt
-                    q.append((cnt, (nx, ny)))
+                    heapq.heappush(q, (cnt, (nx, ny)))
 
 
 solution()
