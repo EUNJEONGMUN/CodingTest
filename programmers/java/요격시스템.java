9de792dd848d2d4f308c90ab1ac2572b84cba962
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-04-26 12:24:01 +0900

    add: programmers 요격 시스템 풀이 완료

---
+++ programmers/java/요격시스템.java
@@ -0,0 +1,33 @@
+package programmers.java;
+
+import java.util.Arrays;
+
+public class 요격시스템 {
+
+    public static void main(String[] args) {
+        요격시스템_Solution solution = new 요격시스템_Solution();
+        int ans = solution.solution(
+            new int[][]{{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}});
+        System.out.println(ans);
+
+    }
+
+}
+
+class 요격시스템_Solution {
+
+    public int solution(int[][] targets) {
+        Arrays.sort(targets, (o1, o2) -> {
+            return Integer.compare(o1[1], o2[1]);
+        });
+        int answer = 1;
+        int time = targets[0][1];
+        for (int[] target : targets) {
+            if (time <= target[0]) {
+                answer++;
+                time = target[1];
+            }
+        }
+        return answer;
+    }
+}
