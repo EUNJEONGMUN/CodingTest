a7d85ddee59cf666db48e8b0d4dc6771d2914ea5
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-05-03 10:40:22 +0900

    add: programmers 연속 펄수 부분수열의 합 풀이 완료

---
+++ programmers/java/연속펄스부분수열의합.java
@@ -0,0 +1,45 @@
+package programmers.java;
+
+public class 연속펄스부분수열의합 {
+
+}
+
+class 연속펄스부분수열의합_Solution {
+
+    public long solution(int[] sequence) {
+        int[] pulse1 = new int[sequence.length];
+        int[] pulse2 = new int[sequence.length];
+
+        for (int i = 0; i < sequence.length; i++) {
+            if (i % 2 == 0) {
+                pulse1[i] = sequence[i];
+                pulse2[i] = sequence[i] * (-1);
+
+            } else {
+                pulse1[i] = sequence[i] * (-1);
+                pulse2[i] = sequence[i];
+            }
+        }
+        return Math.max(getMaxValue(pulse1), getMaxValue(pulse2));
+    }
+
+    private long getMaxValue(int[] arr) {
+        long maxValue = Long.MIN_VALUE;
+        long value = 0;
+        int start = 0;
+        for (int end = 0; end < arr.length; end++) {
+            if (value + arr[end] < 0) {
+                start = end + 1;
+                value = 0;
+                continue;
+            } else {
+                value += arr[end];
+                while (start <= end && arr[start] < 0) {
+                    value -= arr[start++];
+                }
+            }
+            maxValue = Math.max(value, maxValue);
+        }
+        return maxValue;
+    }
+}
